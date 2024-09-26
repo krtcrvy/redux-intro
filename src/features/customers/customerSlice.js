4567f88ae173811d04f8960798abd8ff04ad77c5
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-26 13:12:50 +0800

    Refactor store.js to use redux-devtools-extension and add @reduxjs/toolkit as a dependency

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,10 +1,41 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalId: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = new Date().toISOString();
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/* export default function customerReducer(state = initialState, action) {
   switch (action.type) {
     case "customer/createCustomer":
       return {
@@ -39,4 +70,4 @@ export function updateName(fullName) {
     type: "customer/updateName",
     payload: fullName,
   };
-}
+} */
